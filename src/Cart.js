9fe1af02ade2d4c98e4d7f7989f0130acbd6b07c
jordanrath <rath.jordan@icloud.com>
2022-12-08 14:17:11 -0700

    made changes to Cart.js with Stripe Checkout

--- src/Cart.js
+++ src/Cart.js
@@ -4,7 +4,7 @@ import Input from "./Input.js";
 import Button from "./Button.js";
 
 // TODO: Replace with your own publishable key
-const stripeLoadedPromise = loadStripe("PK_REPLACE_WITH_YOUR_PUBLISHABLE_KEY");
+const stripeLoadedPromise = loadStripe("pk_test_51HsqkCGuhXEITAut89vmc4jtjYd7XPs8hWfo2XPef15MFqI8rCFc8NqQU9WutlUBsd8kmNqHBeEmSrdMMpeEEyfT00KzeVdate");
 
 export default function Cart({ cart }) {
   const totalPrice = cart.reduce(
